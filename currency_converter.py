2d377d5a8e86028971934af146465a0700856696
Julio Leon <juliopena143@icloud.com>
2025-02-11 16:34:39 -0700

    methods convert and print currency

--- currency_converter.py
+++ currency_converter.py
@@ -3,3 +3,60 @@ from pprint import PrettyPrinter
 
 BASE_URL = "https://free.currconv.com/"
 API_KEY = ""
+
+printer = PrettyPrinter()
+
+
+def get_currencies():
+    endpoint = f"api/v7/currencies?apiKey={API_KEY}"
+    url = BASE_URL + endpoint
+    data = get(url).json()['results']
+
+    data = list(data.items())
+    data.sort()
+
+    return data['results']
+
+
+def print_currencies(currencies):
+    for name, currency in currencies:
+        name = currency['currencyName']
+        _id = currency['id']
+        symbol = currency.get("currencySymbol", "")
+        print(f"{_id} - {name} - {symbol}")
+
+
+def exchange_rate(currency1, currency2):
+    endpoint = f"api/v7/convert?q={currency1}_{currency2}&compact=ultra&apiKey={API_KEY}"
+    url = BASE_URL + endpoint
+    data = get(url).json()
+
+    if len(data) == 0:
+        print("Invalid currencies.")
+        return
+
+    rate = list(data.values())[0]
+    print(f"{currency1} => {currency2} = {rate}")
+
+    return rate
+
+
+def convert(currency1, currency2, amount):
+    rate = exchange_rate(currency1, currency2)
+    if rate is None:
+        return
+
+    try:
+        amount = float(amount)
+    except:
+        print("invalid amount")
+        return
+
+    converted_amount = rate * amount
+    print(f"{amount} {currency1} is equal to {converted_amount} {currency2}")
+    return converted_amount
+
+
+data = get_currencies()
+printer.pprint(data)
+rate = exchange_rate("USD", "CAD")
